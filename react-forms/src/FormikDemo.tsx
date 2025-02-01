bea5961f975d33ed38ae2628932c70af4087df49
Diego Balderrama <diegowrhasta@gmail.com>
2025-02-01 09:48:54 -0400

    Adds validation function to Formik form

--- react-forms/src/FormikDemo.tsx
+++ react-forms/src/FormikDemo.tsx
@@ -1,17 +1,48 @@
 import { useFormik } from 'formik'
 
+interface FormProps {
+  firstName: string,
+  lastName: string,
+  email: string
+}
+
+// A custom validation function. This must return an object
+// which keys are symmetrical to our values/initialValues
+const validate = (values: FormProps) => {
+  const errors: Partial<FormProps> = {}
+
+  if (!values.firstName) {
+    errors.firstName = 'Required'
+  } else if (values.firstName.length > 15) {
+    errors.firstName = 'Must be 15 characters or less'
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'Required'
+  } else if (values.lastName.length > 20) {
+    errors.lastName = 'Must be 20 characters or less'
+  }
+
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  return errors
+}
+
 function FormikDemo () {
-  // Pass the useFormik() hook initial form values and a submit function that will
-
+  /// Pass the useFormik() hook initial form values, a validate function that will be called when
+  // form values change or fields are blurred, and a submit function that will
   // be called when the form is submitted
-
   const formik = useFormik({
     initialValues: {
       firstName: '',
       lastName: '',
       email: ''
     },
-
+    validate,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2))
     }
@@ -28,6 +59,7 @@ function FormikDemo () {
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
+      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
 
       <label htmlFor='lastName'>Last Name</label>
 
@@ -38,6 +70,7 @@ function FormikDemo () {
         onChange={formik.handleChange}
         value={formik.values.lastName}
       />
+      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
       <label htmlFor='email'>Email Address</label>
 
@@ -48,6 +81,7 @@ function FormikDemo () {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
       <button type='submit'>Submit</button>
     </form>
