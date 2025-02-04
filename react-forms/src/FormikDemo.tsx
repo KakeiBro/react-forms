0d394fb0bcbade901afd65d8f49d4fd61837515e
Diego Balderrama <diegowrhasta@gmail.com>
2025-02-04 10:34:19 -0400

    Adds final Formik implementation (best practices in mind)

--- react-forms/src/FormikDemo.tsx
+++ react-forms/src/FormikDemo.tsx
@@ -1,5 +1,77 @@
-import { ErrorMessage, Field, Formik } from 'formik'
+import { Form, Formik, useField } from 'formik'
+import { ReactNode } from 'react'
 import * as Yup from 'yup'
+
+interface InputProps {
+  label: string
+  name: string
+  id?: string
+  type?: string
+  placeholder?: string
+}
+
+interface ChildrenInputProps extends InputProps {
+  children: ReactNode
+}
+
+interface CheckboxProps extends Omit<ChildrenInputProps, 'label'> {
+  label?: string
+}
+
+const MyTextInput = ({ label, ...props }: InputProps) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input>. We can use field meta to show an error
+  // message if the field is invalid and it has been touched (i.e. visited)
+  const [field, meta] = useField(props)
+
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+
+      <input className='text-input' {...field} {...props} />
+
+      {meta.touched && meta.error ? (
+        <div className='error'>{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
+
+const MyCheckbox = ({ children, ...props }: CheckboxProps) => {
+  // React treats radios and checkbox inputs differently from other input types:
+  // select and textarea.
+  // Formik does this too! When you specify `type` to useField(), it will
+  // return the correct bag of props for you -- a `checked` prop will be included
+  // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
+  const [field, meta] = useField({ ...props, type: 'checkbox' })
+
+  return (
+    <div>
+      <label className='checkbox-input'>
+        <input type='checkbox' {...field} {...props} />
+        {children}
+      </label>
+
+      {meta.touched && meta.error ? (
+        <div className='error'>{meta.error}</div>
+      ) : null}
+    </div>
+  )
+}
+
+const MySelect = ({ label, ...props }: ChildrenInputProps) => {
+  const [field, meta] = useField(props)
+
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className='error'>{meta.error}</div>
+      ) : null}
+    </div>
+  )
+}
 
 function FormikDemo () {
   return (
@@ -27,23 +99,42 @@ function FormikDemo () {
         }, 400)
       }}
     >
-      {formik => (
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor='firstName'>First Name</label>
-          <Field name='firstName' type='text' />
-          <ErrorMessage name='firstName' />
+      <Form>
+        <MyTextInput
+          label='First Name'
+          name='firstName'
+          type='text'
+          placeholder='Jane'
+        />
 
-          <label htmlFor='lastName'>Last Name</label>
-          <Field name='lastName' type='text' />
-          <ErrorMessage name='lastName' />
+        <MyTextInput
+          label='Last Name'
+          name='lastName'
+          type='text'
+          placeholder='Doe'
+        />
 
-          <label htmlFor='email'>Email Address</label>
-          <Field name='email' type='text' />
-          <ErrorMessage name='email' />
+        <MyTextInput
+          label='Email Address'
+          name='email'
+          type='email'
+          placeholder='jane@formik.com'
+        />
 
-          <button type='submit'>Submit</button>
-        </form>
-      )}
+        <MySelect label='Job Type' name='jobType'>
+          <option value=''>Select a job type</option>
+          <option value='designer'>Designer</option>
+          <option value='development'>Developer</option>
+          <option value='product'>Product Manager</option>
+          <option value='other'>Other</option>
+        </MySelect>
+
+        <MyCheckbox name='acceptedTerms'>
+          I accept the terms and conditions
+        </MyCheckbox>
+
+        <button type='submit'>Submit</button>
+      </Form>
     </Formik>
   )
 }
