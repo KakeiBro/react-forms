7fff4e2d5f8527d9acb5867233befd0339021730
Diego Balderrama <diegowrhasta@gmail.com>
2025-02-05 11:56:46 -0400

    Adds base example for React Hook Form

--- react-forms/src/ReactHookFormDemo.tsx
+++ react-forms/src/ReactHookFormDemo.tsx
@@ -1,5 +1,35 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+interface Inputs {
+  example: string
+  exampleRequired: string
+}
+
 function ReactHookFormDemo () {
-  return <div style={{ flex: 1 }}>DEMO</div>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = console.log
+
+  console.log(watch('example'))
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>React Hook Form</h1>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue='test' {...register('example')} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register('exampleRequired', { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type='submit' />
+    </form>
+  )
 }
 
 export { ReactHookFormDemo }
